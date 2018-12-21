a8cb84f83e43f928042a9a17b7ad39d0af666dbb
92ada <11612325@mail.sustc.edu.cn>
2018-12-22 00:04:56 +0800

    [fix]bug in get file list

--- utils.py
+++ utils.py
@@ -19,6 +19,15 @@ def get_file_list(root):
         else:
             queue.append(path)
 
+def get_seed_path(root, seed):
+    queue = [root]
+    while queue != []:
+        path = queue.pop(0)
+        if os.path.isfile(path):
+            result.append(path)
+        else:
+            queue += os.listdir(path)
+
 def get_ip():
     return ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
 
